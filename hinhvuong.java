bbc02e76c7fbf376f92f88bdc6d65a38a594a7af
Trang16102008 <160478609+Trang16102008@users.noreply.github.com>
2024-10-01 16:34:21 +0700

    Add files via upload

---
+++ hinhvuong.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+
+/**
+ *
+ * @author Administrator
+ */
+  public class hinhvuong extends hinhchunhat {
+    
+    public hinhvuong() {
+        super();
+    }
+    public hinhvuong(int doRong) {
+        super(doRong, doRong);
+    }
+}
